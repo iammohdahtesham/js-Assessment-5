c2fbf88b822a4f2804f3aa52aee9085487d5f974
iammohdahtesham <139335735+iammohdahtesham@users.noreply.github.com>
2023-08-04 11:42:32 +0530

    Update Assessment5.js

--- assessment5.js
+++ assessment5.js
@@ -1,4 +1,76 @@
 // ----------------------------------------------------------------------------------------------
+// Question 5
+// Extract the data from the API https://jsonplaceholder.typicode.com/users and print name, email id, phone number and company name from the extracted data
+// ----------------------------------------------------------------------------------------------
+
+let fetchData = async ()=>{    // creating the function Expression and make it async 
+    try{                                // we put the code in try and catch  to handle the code and error in the program if some error come
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')   //  using await to get the resolved data and using fetch method to fetch the data from the host
+        const users = await data.json()    // passing the fetch data which is promise then convert the data into the json (JavaScript Object Notation) 
+         users.map((value)=>{   // using array inbuild function array.map to itereate the data and getting the expected data 
+           console.log('*********');
+           console.log(`Name :${value.name}`);    
+           console.log(`E-mail:${value.email}`);
+           console.log(`Phone Number :${value.phone}`);
+           console.log(`Company Name :${value.company.name}`);
+           
+         })
+    }catch(error){      // catch method catch the error if any error come 
+    console.log(error);
+    }
+    }
+    fetchData()   // calling the function 
+//OUTPUT - *********
+// Name:Leanne Graham
+// E-mail :Sincere@april.biz
+// Phone Number:1-770-736-8031 x56442
+// Company Name:Romaguera-Crona
+// *********
+// Name:Ervin Howell
+// E-mail :Shanna@melissa.tv
+// Phone Number:010-692-6593 x09125
+// Company Name:Deckow-Crist
+// *********
+// Name:Clementine Bauch
+// E-mail :Nathan@yesenia.net
+// Phone Number:1-463-123-4447
+// Company Name:Romaguera-Jacobson
+// *********
+// Name:Patricia Lebsack
+// E-mail :Julianne.OConner@kory.org
+// Phone Number:493-170-9623 x156
+// Company Name:Robel-Corkery
+// *********
+// Name:Chelsey Dietrich
+// E-mail :Lucio_Hettinger@annie.ca
+// Phone Number:(254)954-1289
+// Company Name:Keebler LLC
+// *********
+// Name:Mrs. Dennis Schulist
+// E-mail :Karley_Dach@jasper.info
+// Phone Number:1-477-935-8478 x6430
+// Company Name:Considine-Lockman
+// *********
+// Name:Kurtis Weissnat
+// E-mail :Telly.Hoeger@billy.biz
+// Phone Number:210.067.6132
+// Company Name:Johns Group
+// *********
+// Name:Nicholas Runolfsdottir V
+// E-mail :Sherwood@rosamond.me
+// Phone Number:586.493.6943 x140
+// Company Name:Abernathy Group
+// *********
+// Name:Glenna Reichert
+// E-mail :Chaim_McDermott@dana.io
+// Phone Number:(775)976-6794 x41206
+// Company Name:Yost and Sons
+// *********
+// Name:Clementina DuBuque
+// E-mail :Rey.Padberg@karina.biz
+// Phone Number:024-648-3804
+// Company Name:Hoeger LLC
+// ----------------------------------------------------------------------------------------------
 // Question 6 
 // Explain and write a code snippet for the following:-
 // a).  Promise.all()
